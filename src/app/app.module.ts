9acc324e9df4b54a8c268169b40e668f9211dbc6
DavidKerns <davidkerns@yahoo.com>
2017-11-08 19:54:53 -0500

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { MyHomeComponent } from './my-home/my-home.component';
+import { MyAboutComponent } from './my-about/my-about.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule }   from '@angular/forms';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home',  component: MyHomeComponent },
+  { path: 'about', component: MyAboutComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHomeComponent,
+    MyAboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+ RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
